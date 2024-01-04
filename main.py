bc682d184b1472950cc86aea04de5713cd05ad7d
Marko Visnjic <markovisnjic12@gmail.com>
2024-01-03 21:38:41 -0500

    First Working Version

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class HomePage(MethodView):
 
 class BillForm(FlaskForm):
     amount = StringField("Bill Amount: ")
-    period = StringField("Bill Period: ")
+    period = StringField("Bill Period (E.g. March 2020): ")
 
     name1= StringField("Name: ")
     days_in_house1 = StringField("Days in the house: ")
@@ -31,29 +31,32 @@ class BillFormPage(MethodView):
     def get(self):
         bill_form = BillForm()
         return render_template('bill_form_page.html',
-                               billform=bill_form, )
+                               billform=bill_form)
 
 
 class ResultsPage(MethodView):
     def post(self):
-        billform = BillForm(request.form)
+        try:
+            billform = BillForm(request.form)
 
-        amount = int(billform.amount.data)
-        period = str(billform.period.data)
-        bill = Bill(amount=amount, period=period)
+            amount = int(billform.amount.data)
+            period = str(billform.period.data)
+            bill = Bill(amount=amount, period=period)
 
-        flatmate1_name = str(billform.name1.data)
-        flatmate1_days = int(billform.days_in_house1.data)
-        flatmate1 = Flatmate(flatmate1_name,flatmate1_days)
+            flatmate1_name = str(billform.name1.data)
+            flatmate1_days = int(billform.days_in_house1.data)
+            flatmate1 = Flatmate(flatmate1_name,flatmate1_days)
 
-        flatmate2_name = str(billform.name2.data)
-        flatmate2_days = int(billform.days_in_house2.data)
-        flatmate2 = Flatmate(flatmate2_name,flatmate2_days)
+            flatmate2_name = str(billform.name2.data)
+            flatmate2_days = int(billform.days_in_house2.data)
+            flatmate2 = Flatmate(flatmate2_name,flatmate2_days)
 
-        flatmate_report = PdfReport('report.pdf')
-        flatmate_report_url = flatmate_report.generate(flatmate1, flatmate2, bill)
+            flatmate_report = PdfReport('report.pdf')
+            flatmate_report_url = flatmate_report.generate(flatmate1, flatmate2, bill)
 
-        return f"Process done! View the bill here: {flatmate_report_url}"
+            return render_template('results_page.html', link=flatmate_report_url)
+        except:
+            return "Error occured. Please try again."
 
 
 
